ba3fd9ab335767b687b80f124a1b5e0eae136a4d
romagritsuk <30575084+romagritsuk@users.noreply.github.com>
2018-01-05 20:59:22 +0300

    popup work

--- js/popup.js
+++ js/popup.js
@@ -1,25 +1,20 @@
-var btn = document.querySelector('.button-content-map');
-var id = btn.getAttribute('href');
-var popup = document.getElementById(id);
-var closeBtn = document.querySelector('.close-element');
-btn.addEventListener('click', function (e) {
-  e.preventDefault();
-  popup.classList.add('active');
-    console.log(popup); 
-});
-
-function popupClose() {
-  popup.classList.remove('active');
-}
-
-closeBtn.addEventListener('click', function () {
-  popupClose();
-});
-
-window.addEventListener('keydown', function (e) {
-  if (e.keyCode === 27) {
-    if (popup.classList.contains('active')) {
-      popupClose();
+$('.popupbtn').click(
+    function(event){
+        var id = $(this).attr('href');
+        $('#'+id).addClass("active");
+        $('.dark-bg').addClass("active");
     }
+)
+
+$('.close-pop').click(
+    function(event){
+        $('.popup').removeClass("active");
+        $('.dark-bg').removeClass("active");
+    }
+)
+$('html').keydown(function(e){
+  if (e.keyCode == 27) {
+    $('.popup').removeClass("active");
+    $('.dark-bg').removeClass("active");
   }
 });
